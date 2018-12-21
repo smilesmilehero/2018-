050a58d98487d9a82b93f461ca8672d2e6b2c35f
Ruilong25 <105360025@ntut.org.tw>
2018-12-21 14:45:55 +0800

    change3
    0231

--- app/src/main/java/com/example/jordy/tarot/MainActivity1.java
+++ app/src/main/java/com/example/jordy/tarot/MainActivity1.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -20,14 +21,44 @@ public class MainActivity1 extends AppCompatActivity {
     EditText CCCC;
     EditText MM;
     EditText DD;
-    Integer name2, CCCCss, MMss, DDss;
-
+    Integer name2, CCCCss, MMss, DDss,se=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
+
+
+        RadioGroup rg1 = (RadioGroup) findViewById(R.id.radioGroup);
+        rg1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId)
+                {
+<<<<<<< HEAD
+                    case R.id.radioButton4:
+                        se=1;
+                        break;
+                    case R.id.radioButton5:
+                        se=2;
+                        break;
+                    case R.id.radioButton6:
+=======
+                    case R.id.radioButton:
+                        se=1;
+                        break;
+                    case R.id.radioButton2:
+                        se=2;
+                        break;
+                    case R.id.radioButton3:
+>>>>>>> changed1
+                        se=3;
+                        break;
+
+                }
+            }
+        });
 
         name=(EditText) findViewById(R.id.name);
         CCCC=(EditText) findViewById(R.id.CCCC);
@@ -48,8 +79,8 @@ public class MainActivity1 extends AppCompatActivity {
 
 
 
-       if (names.isEmpty()||names.equals(".")||CCCCs.isEmpty()||CCCCs.equals(".")||
-               MMs.isEmpty()||MMs.equals(".")||DDs.isEmpty()||DDs.equals("."))
+        if (names.isEmpty()||names.equals(".")||CCCCs.isEmpty()||CCCCs.equals(".")||
+                MMs.isEmpty()||MMs.equals(".")||DDs.isEmpty()||DDs.equals(".")||se==0)
         {
             Toast.makeText(this,"請確認是否值皆有填",Toast.LENGTH_SHORT).show();
         }
@@ -64,36 +95,31 @@ public class MainActivity1 extends AppCompatActivity {
                Toast.makeText(this, "有錯誤", Toast.LENGTH_SHORT).show();
            }*/
         else {
-                //name2 = Integer.parseInt(names);
-                CCCCss = Integer.parseInt(CCCCs);
-                MMss = Integer.parseInt(MMs);
-                DDss = Integer.parseInt(DDs);
-                if ((MMss>12) || (1>MMss)||(1>DDss)||(DDss>31)||((MMss==2)&&(DDss>29))) {
-                    Toast.makeText(this, "輸入值有錯誤", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Intent it = new Intent(this, MainActivity2.class);
-                    int sum;
-                    int suma;
-                   // int sumb;
-                   // int rb;
-                   // rb = (int)(Math.random())*154;
-                    //rc =(int)(Math.random())*77;
-                   // suma = (CCCCss*rb) * (MMss * DDss);
-                    Random x = new Random();
-                    sum = x.nextInt(155)+1;
+            //name2 = Integer.parseInt(names);
+            CCCCss = Integer.parseInt(CCCCs);
+            MMss = Integer.parseInt(MMs);
+            DDss = Integer.parseInt(DDs);
+            if ((MMss>12) || (1>MMss)||(1>DDss)||(DDss>31)||((MMss==2)&&(DDss>29))) {
+                Toast.makeText(this, "輸入值有錯誤", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Intent it = new Intent(this, MainActivity2.class);
+                int sum;
+                int suma;
+                int sumb;
+                sumb = (int)(Math.random()* 87);
+                suma = CCCCss + MMss + DDss + sumb;
+                //  Random x = new Random();
+                //   sumb = x.nextInt(78);
 
+                sum= (suma*se) % 155 + 1;
 
-                   // sum= suma % 155  ;
-
-
-                        Bundle bundle = new Bundle();
-                        bundle.putInt("SUM", sum);
-                        it.putExtras(bundle);
-                        startActivityForResult(it, 100);
-
-                }
-           }
+                Bundle bundle=new Bundle();
+                bundle.putInt("SUM",sum);
+                it.putExtras(bundle);
+                startActivityForResult(it, 100);
+            }
+        }
 
     }
 }
